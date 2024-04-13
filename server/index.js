028f2d739e9569877b3b52403023f36a74624bee
vishalvardhan02 <124067147+vishalvardhan02@users.noreply.github.com>
2024-04-13 15:11:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,12 +21,6 @@ database.connect();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-	cors({
-		origin:"http://localhost:3000",
-		credentials:true,
-	})
-)
 
 app.use(
 	fileUpload({
